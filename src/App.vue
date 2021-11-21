65566e4d079f71122aa8627d1b81bbb9fe8743c2
VitorSilvaBueno <vitot770@gmail.com>
2021-11-21 00:34:57 -0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <template-avaliacao />
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld";
+import TemplateAvaliacao from "./components/TemplateAvaliacao.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    TemplateAvaliacao,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/TemplateAvaliacao.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-container fill-height>
+    <v-system-bar app>
+      <v-row class="text-center" align="center" justify="center">
+        <v-col cols="12"> {{ navegacaoSuperior[0].text }} </v-col>
+      </v-row>
+    </v-system-bar>
+    <v-app-bar app clipped-left>
+      <v-row class="text-center">
+        <v-col cols="12"> {{ navegacaoSuperior[1].text }} </v-col>
+      </v-row>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" clipped permanent app>
+      <v-container fluid fill-height>
+        <v-row class="text-center">
+          <v-col cols="12"> {{ navegacaoCentral[0].text }} </v-col>
+        </v-row>
+      </v-container>
+    </v-navigation-drawer>
+    <v-content fill-height>
+      <v-container fill-height></v-container>
+      <v-row class="text-center">
+        <v-col cols="12"> {{ navegacaoCentral[1].text }} </v-col>
+      </v-row>
+    </v-content>
+    <v-footer app>
+      <v-row class="text-center">
+        <v-col cols="12"> {{ navegacaoInferior[0].text }} </v-col>
+      </v-row>
+    </v-footer>
+    <v-bottom-navigation app>
+      <v-container fluid fill-height>
+        <v-row class="text-center">
+          <v-col cols="12"> {{ navegacaoInferior[1].text }} </v-col>
+        </v-row>
+      </v-container>
+    </v-bottom-navigation>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "TemplateAvaliacao",
+  data: () => ({
+    navegacaoSuperior: [
+      {
+        text: "v-system-bar",
+      },
+      {
+        text: "v-app-bar",
+      },
+    ],
+    navegacaoCentral: [
+      {
+        text: "v-navigation-drawer",
+      },
+      {
+        text: "v-content",
+      },
+    ],
+    navegacaoInferior: [
+      {
+        text: "v-footer",
+      },
+      {
+        text: "v-bottom-navigation",
+      },
+    ],
+  }),
+};
+</script>
